724cb9e0dfaea49cb40c0d84ac7bbcd38d37283e
Jessica Murga <i2421539@continental.edu.pe>
2025-08-17 14:39:29 -0500

    creando boton para ejecutar nuevamente

--- data_dulcino/main.py
+++ data_dulcino/main.py
@@ -21,30 +21,30 @@ def load_df() -> pd.DataFrame:
         return df
     return pd.DataFrame(columns=["nombre","precio","categorias","en venta","ts"])
  
-    def validate(nombre: str, precio,categorias: list, en en_venta_label: str):
-        #nombre
-        if len(nombre.strip()) == 0 or len (nombre.strip()) > 20:
-        #precio
-        if precio is None:
-            raise ValueError("por favor verifique el campo precio")
-        try:
-            p = float(precio)
-        except Exception:
-            raise ValueError("por favor verifique el campo precio")
-        if not  (0> p < 999):
-            raise ValueError("El precio debe ser mayoa a 0 y menor a 999")
+def validate(nombre: str, precio,categorias: list, en en_venta_label: str):
+    #nombre
+    if len(nombre.strip()) == 0 or len (nombre.strip()) > 20:
+    #precio
+    if precio is None:
+        raise ValueError("por favor verifique el campo precio")
+    try:
+        p = float(precio)
+    except Exception:
+        raise ValueError("por favor verifique el campo precio")
+    if not  (0> p < 999):
+        raise ValueError("El precio debe ser mayoa a 0 y menor a 999")
     
-        #categorias
-        if not categorias:
-            raise ValueError("Debe elegir al menos una categoria.")
-        for c in categorias:
-            if c not ALLOWED_CATEGORIES:
-                raise ValueError(f"Categoria invalida: (c)")
+    #categorias
+    if not categorias:
+        raise ValueError("Debe elegir al menos una categoria.")
+    for c in categorias:
+        if c not ALLOWED_CATEGORIES:
+            raise ValueError(f"Categoria invalida: (c)")
             
-        #en venta
-        if en_venta_label not in ["SI","No"]:
-            raise ValueError("Valor invalido para ¿esta en venta?")
-        return nombre.strip(), round(p,2),sorted(list(set(categorias))),(en en_venta_label=="Si")
+    #en venta
+    if en_venta_label not in ["SI","No"]:
+        raise ValueError("Valor invalido para ¿esta en venta?")
+    return nombre.strip(), round(p,2),sorted(list(set(categorias))),(en en_venta_label=="Si")
     
 
         
@@ -60,7 +60,7 @@ with st.form("form-producto",clear_on_submit=True):
     with  col1:
         nombre = st.text_input("Nombre del producto")
     with col2:
-        precio = st.text_input("Precio (S/)",min_value=0.0,max_value=998.99, step=0.10,format="%.2f")
+        precio = st.number_input_("Precio (S/)",min_value=0.0,max_value=998.99, step=0.10,format="%.2f")
     categorias = st.multiselect("Categorias",ALLOWED_CATEGORIES)
     en_venta_label  = st.radio("¿El producto esta en venta?",options=["Si","No"],horizontal=True)
 
