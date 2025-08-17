114a9eb37e78b263e99ae5594e3ed5e3d282290d
Jessica Murga <i2421539@continental.edu.pe>
2025-08-17 17:18:50 -0500

    creando boton para ejecutar nuevamente 3

--- data_dulcino/main.py
+++ data_dulcino/main.py
@@ -17,13 +17,14 @@ def ensure_dir():
 
 def load_df() -> pd.DataFrame:
     if os.path.exists(CSV_PATH):
-        df = pd.read_csv(CSV_PATH,encoding="utf-8")
-        return df
+        return pd.read_csv(CSV_PATH,encoding="utf-8")
     return pd.DataFrame(columns=["nombre","precio","categorias","en venta","ts"])
  
-def validate(nombre: str, precio,categorias: list, en en_venta_label: str):
+def validate(nombre: str, precio,categorias: list, en_venta_label: str):
     #nombre
-    if len(nombre.strip()) == 0 or len (nombre.strip()) > 20:
+    nombre = nombre.strip()
+    if len(nombre) == 0 or len(nombre) > 20:
+        raise ValueError("El nombre debe tener entre 1 y 20 caracteres")
     #precio
     if precio is None:
         raise ValueError("por favor verifique el campo precio")
@@ -31,20 +32,21 @@ def validate(nombre: str, precio,categorias: list, en en_venta_label: str):
         p = float(precio)
     except Exception:
         raise ValueError("por favor verifique el campo precio")
-    if not  (0> p < 999):
-        raise ValueError("El precio debe ser mayoa a 0 y menor a 999")
+    if not  (0 < p < 999):
+        raise ValueError("El precio debe ser mayor a 0 y menor a 999")
     
     #categorias
     if not categorias:
         raise ValueError("Debe elegir al menos una categoria.")
     for c in categorias:
-        if c not ALLOWED_CATEGORIES:
-            raise ValueError(f"Categoria invalida: (c)")
+        if c not in ALLOWED_CATEGORIES:
+            raise ValueError(f"Categoria invalida: {c}")
             
     #en venta
     if en_venta_label not in ["SI","No"]:
         raise ValueError("Valor invalido para ¿esta en venta?")
-    return nombre.strip(), round(p,2),sorted(list(set(categorias))),(en en_venta_label=="Si")
+    
+    return nombre, round(p, 2), sorted(set(categorias)), (en_venta_label == "Si")
     
 
         
@@ -53,19 +55,22 @@ def validate(nombre: str, precio,categorias: list, en en_venta_label: str):
 
 #-----------------------------------UI---------------------------------------
 
-st.title("confiteria Dulcino - Registro de Productos")
+ensure_dir()
+df = load_df()
 
-with st.form("form-producto",clear_on_submit=True):
-    col1,col2 = st.columns([2,1])
-    with  col1:
+st.subheader("Registro de Productos")
+
+with st.form("form-producto", clear_on_submit=True):
+    col1, col2 = st.columns([2, 1])
+    with col1:
         nombre = st.text_input("Nombre del producto")
     with col2:
-        precio = st.number_input_("Precio (S/)",min_value=0.0,max_value=998.99, step=0.10,format="%.2f")
-    categorias = st.multiselect("Categorias",ALLOWED_CATEGORIES)
-    en_venta_label  = st.radio("¿El producto esta en venta?",options=["Si","No"],horizontal=True)
+        precio = st.number_input("Precio (S/)", min_value=0.0, max_value=998.99, step=0.10, format="%.2f")
+    
+    categorias = st.multiselect("Categorías", ALLOWED_CATEGORIES)
+    en_venta_label = st.radio("¿El producto está en venta?", options=["Si", "No"], horizontal=True)
 
     submitted = st.form_submit_button("Guardar")
-
 
     if submitted:
         try:
@@ -82,6 +87,7 @@ with st.form("form-producto",clear_on_submit=True):
             st.success("✅ Producto guardado correctamente.")
         except Exception as e:
             st.error(f"❌ Error: {e}")
+            
 
 
 
